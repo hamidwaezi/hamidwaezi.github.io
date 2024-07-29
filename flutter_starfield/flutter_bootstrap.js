a0814efc9d3d043a60331652448c185525203387
Hamid Waezi <waezi.h@gmail.com>
2024-07-29 21:27:45 +0100

    update star field app

--- flutter_starfield/flutter_bootstrap.js
+++ flutter_starfield/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4068327139"
+    serviceWorkerVersion: "1060583545"
   }
 });
